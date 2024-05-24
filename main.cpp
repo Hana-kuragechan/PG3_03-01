0933d7e70665664cafb3d91c794ff729f18f882d
Hana-kuragechan <k023g1049@g.neec.ac.jp>
2024-05-24 13:55:22 +0900

    03-01確認課題

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
-#include<stdio.h>
-
+﻿#include<stdio.h>
+#include<list>
+#include<vector>
 int main() {
+	std::list<const char*>lst{ 
+		"Tokyo","Kanda","Akihabara","Okachimachi","Ueno","Uguisudani","Nippori","Tabata","Komagome"
+		"Sugamo","Otuka","Ikebukuro","Mejiro","Takadanobaba","Shin-Okubo","Shinjuku","Yoyogi","Harajuku","Shibuya",
+		"Ebisu","Meguro","Gotanda","Osaki","Shinagawa","Tamachi","Hamamatsucho","Shimbashi","Yurakucho"
+	};
 
+	printf("1970年以前の駅\n");
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+		
+		printf("%s", *itr);
+	}
 
+	printf("\n1970年の駅\n");
+	for (auto itr = lst.begin();  itr !=lst.end(); ++itr) {
+		if (*itr == "Tabata") {
+			itr = lst.insert(itr, "Nishi-Nippori");
+			printf("%s", *itr);
+			++itr;
+		}
+		printf("%s", *itr);
+	}
+	printf("\n2020年の駅\n");
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+		if (*itr == "Tamachi") {
+			itr = lst.insert(itr, "Takanawa Gateway");
+			printf("%s", *itr);
+			++itr;
+		}
+		printf("%s", *itr);
+	}
 	return 0;
 }

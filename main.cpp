86b4139b5ed1fd34c3aa1743159127f776147955
Hana-kuragechan <k023g1049@g.neec.ac.jp>
2024-06-07 16:00:27 +0900

    04_02確認課題完了

--- main.cpp
+++ main.cpp
@@ -1,20 +1,20 @@
 ﻿#include<stdio.h>
 
-template<typename Type>
+template<typename Type1, typename Type2>
 class Number
 {
 public:
-	Type a;
-	Type b;
+	Type1 a;
+	Type2 b;
 
-	Number(Type a, Type b) :a(a), b(b) {};
+	 Number(Type1 a, Type2 b) :a(a), b(b) {};
 
-	Type Min() {
+	Type1 Min() {
 		if (a > b) {
-			return static_cast<Type>(b);
+			return static_cast<Type1>(b);
 		}
 		else if (a < b) {
-			return static_cast<Type>(a);
+			return static_cast<Type1>(a);
 		}
 	}
 private:
@@ -24,15 +24,24 @@ private:
 
 int main() {
 	
-	///int型
-	Number<int>num1(10, 20);
-	///float型
-	Number<float>num2(8.2f, 3.3f);
-	///double型
-	Number<float>num3(10.2, 6.7);
+	///戻り値int
+	Number<int,float>num1(10, 20.4f);
+	Number<int, double>num2(10, 6.7);
+	///戻り値float
+	Number<float,double>num3(8.2f, 3.3);
+	Number<float,int>num4(8.2f, 3);
+	///戻り値double
+	Number<double,int>num5(7.9, 8);
+	Number<double,float>num6(3.7, 6.8f);
+	
 
 	printf("%d\n", num1.Min());
-	printf("%f\n", num2.Min());
-	printf("%lf\n", num3.Min());
+	printf("%d\n", num2.Min());
+
+	printf("%f\n", num3.Min());
+	printf("%f\n", num4.Min());
+
+	printf("%lf\n", num5.Min());
+	printf("%lf\n", num6.Min());
 	return 0;
 }
